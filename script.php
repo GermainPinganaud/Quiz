8c6e524ae8019bc26c5f0c604b5d61033f99ec55
admin <admin@iMac3.local>
2017-10-26 10:30:06 +0200

    first commit

---
+++ script.php
@@ -0,0 +1,11 @@
+<?php
+$points = 0;
+$points += $_GET["question_1"];
+$points += $_GET["question_2"];
+$points += $_GET["question_3"];
+$points += $_GET["question_4"];
+if ($_GET["question_5"] == "18") {
+  $points += 3;
+}
+
+echo "Tu as $points points";
