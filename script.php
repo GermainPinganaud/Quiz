f61db690f0f0f3a50effcff7b750a6fb28e0497b
admin <admin@iMac3.local>
2017-10-26 11:57:11 +0200

    backup avant template

--- script.php
+++ script.php
@@ -1,5 +1,8 @@
 <?php
 $points = 0;
+if (isset($_GET) == false) {
+  $points += 0;
+}
 $points += $_GET["question_1"];
 $points += $_GET["question_2"];
 $points += $_GET["question_3"];
@@ -7,5 +10,4 @@ $points += $_GET["question_4"];
 if ($_GET["question_5"] == "18") {
   $points += 3;
 }
-
 echo "Tu as $points points";
