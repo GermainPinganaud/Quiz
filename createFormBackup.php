52036104ffff70b7e96bca40cdcb8880594bb768
admin <admin@iMac3.local>
2017-10-26 17:41:08 +0200

    travail arreté a la fin du cours

--- createFormBackup.php
+++ createFormBackup.php
@@ -11,16 +11,11 @@ function closeTag($tag)
 {
   return "</" . $tag . ">";
 }
-
-
-
 //Mise en place d'un input
   //Type = string
   //Name = string
   //Value = string
   //Label = string              functionName($label = $name) {} ?
-
-
 function createTitle($class = "#", $label)
 {
   return "<h3 class='" . $class . "'>" . $label . "</h3>";
@@ -29,31 +24,23 @@ function createInput($type, $name, $value = "no value", $label = "no label")
 {
   return "<input type='" . $type . "' name='" . $name . "' value='" . $value . "'><label>" . $label . "</label><br>";
 }
-
-
-
-
-
-//Mise en place d'un selectRadio
-
-
-function createSelectRadio($label, $subtitle, $options)
-{
-  $html = "<label>" . $label . "</label>"; //Creer une variable string permet la concatenation sur plusieurs lignes
-  $html .= "<p>" . $subtitle . "</p>";
-  foreach ($options as $key => $value)
-  {
-    $html .= "<input type='radio'" . $value["value"] . "'>" . $value['content'] . "</radio>";
-  }
-  $html .= "</select>";
-  return $html;
-}
-
 //Mise en place d'un select
-
-
 function createSelect($name, $label, $options)
 {
+  // $options = [
+  //   [
+  //     "value" => "fr",
+  //     "content" => "Français",
+  //   ],
+  //   [
+  //     "value" => "en",
+  //     "content" => "English",
+  //   ],
+  //   [
+  //     "value" => "de",
+  //     "content" => "Deutsch",
+  //   ],
+  // ];
   $html = "<label>" . $label . "</label>"; //Creer une variable string permet la concatenation sur plusieurs lignes
   $html .= "<select name='" . $name . "'>";
   foreach ($options as $key => $value)
@@ -63,12 +50,7 @@ function createSelect($name, $label, $options)
   $html .= "</select>";
   return $html;
 }
-
-
-
 //Generer un bouton submit
-
-
 function createSubmit($value)
 {
   return "<input type='submit' value='" . $value . "'>";
--- index.php
+++ index.php
@@ -2,30 +2,71 @@
   require_once('./createForm.php');
 ?>
 <!DOCTYPE html>
-<html>
+<html lang="fr">
   <head>
     <meta charset="utf-8">
-    <title></title>
+    <title>Quizz</title>
+    <link rel="stylesheet" href="style.css">
+    <link href="https://fonts.googleapis.com/css?family=Gochi+Hand|Sigmar+One" rel="stylesheet">
   </head>
   <body>
+    <h1>Quiz Jap<span class="red_o">o</span>n</h1>
+    <!-- FIN DU HEAD -->
     <?php
-      echo openForm("./monScript.php");
-      echo createSelect("country", "Choisissez un pays", [
+      echo openForm("./submit.php");
+      // echo createInput("$type", "name", "value", "label");
+
+      //Question 1
+      // function createSelect($name, $label, $options)
+      createSelectradio("Quelle langue est parlée au Japon ?",
+      [
         [
-          "value" => "es",
-          "content" => "Espagne",
+          "value" => "0",
+          "content" => "Le Chinois",
         ],
         [
-          "value" => "jp",
-          "content" => "Japon",
+          "value" => "0",
+          "content" => "Le Sushi",
         ],
-      ]);
-      echo createInput("text", "myText", "", "Mon premier input");
-      echo createInput("number", "myNumber", "", "Mon second input");
-      echo createInput("radio", "myRadio", "", "Ma radio (du zizi)");
-      echo createInput("checkbox", "myCheckbox", "", "Ma checkbox");
+        [
+          "value" => "1",
+          "content" => "Le Japonais",
+        ],
+        [
+          "value" => "0",
+          "content" => "La réponse D",
+        ],
+      ],);
+
+
+
+
+
+
+
+
+
+
+
+
+
+      //Fermeture du form
       echo closeTag("form");
       echo createSubmit("Envoyer");
     ?>
   </body>
 </html>
+
+
+
+
+
+
+
+
+
+// echo createTitle("", "Quelle langue est parlée au Japon ?");
+// echo createInput("radio", "question_1", "", "Le Chinois");
+// echo createInput("radio", "question_1", "", "Le Sushi");
+// echo createInput("radio", "question_1", "", "Le Japonais");
+// echo createInput("radio", "question_1", "", "Zhongguo");
