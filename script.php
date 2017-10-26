c153d498d4a123e0c60e70e283b26d4f0b4a9966
admin <admin@iMac3.local>
2017-10-26 14:04:37 +0200

    templating efficace

--- script.php
+++ script.php
@@ -1,8 +1,20 @@
 <?php
 $points = 0;
-if (isset($_GET) == false) {
-  $points += 0;
-}
+
+// if (!isset($_GET["question_1"])) {$points += 0;}
+// if (!isset($_GET["question_2"])) {$points += 0;}
+// if (!isset($_GET["question_3"])) {$points += 0;}
+// if (!isset($_GET["question_4"])) {$points += 0;}
+// if (!isset($_GET["question_5"])) {$points += 0;}
+// if (!isset($_GET["question_6"])) {$points += 0;}
+
+!isset($_GET["question_1"]) ? $points += 0 : $points += 0 ;
+!isset($_GET["question_2"]) ? $points += 0 : $points += 0 ;
+!isset($_GET["question_3"]) ? $points += 0 : $points += 0 ;
+!isset($_GET["question_4"]) ? $points += 0 : $points += 0 ;
+!isset($_GET["question_5"]) ? $points += 0 : $points += 0 ;
+!isset($_GET["question_6"]) ? $points += 0 : $points += 0 ;
+
 $points += $_GET["question_1"];
 $points += $_GET["question_2"];
 $points += $_GET["question_3"];
@@ -10,4 +22,8 @@ $points += $_GET["question_4"];
 if ($_GET["question_5"] == "18") {
   $points += 3;
 }
+$points += $_GET["question_6"];
+
+require_once("./head.html");
 echo "Tu as $points points";
+require_once("./foot.html");
